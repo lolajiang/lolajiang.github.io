900bf56012af91348980db892b0ce8a1ff1e3bc7
Lola Jiang <hjwwjxm@umich.edu>
2018-01-11 07:58:13 -0500

    new homepage

--- javascript/script.js
+++ javascript/script.js
@@ -20,31 +20,31 @@ $(function(){
 
 
 // scroll change color
-$(window).scroll(function() {
-    containeR = window;
-    containeD = document;
-    var scrollPercent = 100 * $(containeR).scrollTop() / ($(containeD).height() - $(containeR).height());
-    var scroll = $(window).scrollTop();
-
-    if (scrollPercent > 25 ) {
-       $(".scrollchange").css("color", "#f3f3f3");
-      $(".current").css("border-bottom", "1px solid #f3f3f3");
-      $(".hamburger-line").css("background", "#fff");
-       $(".scrollchange").mouseenter(function(){
-  $(this).css("border-bottom", "1px solid #f3f3f3");
-       })
-         .mouseleave(function(){
-  $(this).css("border-bottom", "1px solid rgba(0,0,0,0)");
-});
-    } else {
-       $(".scrollchange").css("color", "#000000");
-       $(".current").css("border-bottom", "1px solid #000");
-      $(".hamburger-line").css("background", "rgb(0,40,60)");
-       $(".scrollchange").mouseenter(function(){
-  $(this).css("border-bottom", "1px solid #000000");
-       })
-         .mouseleave(function(){
-  $(this).css("border-bottom", "1px solid rgba(0,0,0,0)");
-})
-    }
-});
+// $(window).scroll(function() {
+//     containeR = window;
+//     containeD = document;
+//     var scrollPercent = 100 * $(containeR).scrollTop() / ($(containeD).height() - $(containeR).height());
+//     var scroll = $(window).scrollTop();
+//
+//     if (scrollPercent > 25 ) {
+//        $(".scrollchange").css("color", "#f3f3f3");
+//       $(".current").css("border-bottom", "1px solid #f3f3f3");
+//       $(".hamburger-line").css("background", "#fff");
+//        $(".scrollchange").mouseenter(function(){
+//   $(this).css("border-bottom", "1px solid #f3f3f3");
+//        })
+//          .mouseleave(function(){
+//   $(this).css("border-bottom", "1px solid rgba(0,0,0,0)");
+// });
+//     } else {
+//        $(".scrollchange").css("color", "#000000");
+//        $(".current").css("border-bottom", "1px solid #000");
+//       $(".hamburger-line").css("background", "rgb(0,40,60)");
+//        $(".scrollchange").mouseenter(function(){
+//   $(this).css("border-bottom", "1px solid #000000");
+//        })
+//          .mouseleave(function(){
+//   $(this).css("border-bottom", "1px solid rgba(0,0,0,0)");
+// })
+//     }
+// });
